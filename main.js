888778a41cecf0c4e4d7fe11eaec8e731fab5013
Clyde Alegro <clyde.alegro@goabroad.com>
2016-04-04 18:27:19 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,11 +1,24 @@
-//	Expressions : a fragment of code that produces a value
-// 28  
-// true
-// xyz
-console.log(28);
+// Variables
 
-// Statement : simplest form is an expression followed by a semicolon
-// true;
-// x = 0;
-console.log(!false);
+// Variable declaration
+var x; // declared an undefined variable x
+
+// Declaring a variable and setting a value to it
+// var y = 0;
+// var word = "i am a word";
+// var state = false;
+
+// Using a declared variable in an expression
+// var number = 5;
+// console.log(number + number);
+
+// Variables can refer to the same value
+// var savings = 1000;
+// savings = savings - 100;
+// console.log(savings);
+
+// Defining multiple variables using one var statement
+// var x = 0,
+// 	   y = 2,
+// 	   z = 100;
 
