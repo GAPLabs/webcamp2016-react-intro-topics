1cef2e48b188f15d8127fb21b453bf1710a7f5a2
Clyde Alegro <clyde.alegro@goabroad.com>
2016-04-04 14:07:04 +0800

    initial commit

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
-// type your code here
-console.log("Hello World of JS from the external file!!!");
+//	Expressions : a fragment of code that produces a value
+// 28  
+// true
+// xyz
+console.log(28);
+
+// Statement : simplest form is an expression followed by a semicolon
+// true;
+// x = 0;
+console.log(!false);
+
