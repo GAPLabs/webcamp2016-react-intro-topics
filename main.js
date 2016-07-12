f8d3099655586893d79c361f8f7a7eb1202548ec
Clyde Alegro <clyde.alegro@goabroad.com>
2016-07-12 14:33:04 +0800

    added function code for presentation

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
 // Functions
+
+// Function Declaration
+// function cube(num) {
+// 	console.log(num * num * num);
+// }
+
+// Function Expression Declaration
+// var cube = function(num) {
+// 	console.log(num * num * num);
+// }
+
+// Functions can have multiple parameters
+// function formula(a, b, c) {
+// 	console.log(a + b + c);
+// }
+
+// Function parameter and scopes
+// var a = 1;
+
+// var f1 = function(a) {
+// 	var a = a + a;
+// 	console.log(a);
+// }
+
+// f1();
+
+// var f2 = function(a) {
+// 	var a = a * a;
+// 	console.log(a);
+// }
+
+// f2();
+
+// Optional Arguments
+// function power(base, exponent) {
+// 	if (exponent == undefined)
+// 		exponent = 2;
+
+// 	var result = 1;
+	
+// 	for(var count = 0; count < exponent; count++) {
+// 		result *= base;
+// 	}
+
+// 	return result;
+// }
+
+// console.log(power(4));
+// console.log(power(4,3));
+
+
+
