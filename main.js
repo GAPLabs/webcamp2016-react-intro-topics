2788ad89e24a577807947e9a4a26044fdf72e8f6
Clyde Alegro <clyde.alegro@goabroad.com>
2016-07-15 16:27:43 +0800

    initial commit on this branch

--- main.js
+++ main.js
@@ -1,14 +1,2 @@
-// Document Object Model
-// console.log(document);
-
-// Finding Elements
-// var form = document.getElementById("myform");
-// console.log(form);
-
-// var p = document.getElementsByTagName("p");
-// console.log(p); // result is an array
-// console.log(p[0])
-
-// var link = document.querySelector(".mylink");
-// console.log(link);
+// Event Handling
 
