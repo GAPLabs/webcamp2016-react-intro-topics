44748c400bca8889ab8bf7d051761cc6a35aa34b
Clyde Alegro <clyde.alegro@goabroad.com>
2016-07-15 21:14:06 +0800

    added event handling

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 // Event Handling
 
+// Adding a click event on a button
+// var button = document.querySelector("button");
+// button.addEventListener("click", function(){
+// 	console.log("I have clicked this button!!!");
+// });
+
+// Default actions
+// var link = document.querySelector("a");
+
+// link.addEventListener("click", function(event){
+// 	event.preventDefault();
+// 	console.log("I have just clicked a link!!!");
+// });
+
+// Key events
+// addEventListener("keydown", function(event){
+// 	if(event.keyCode == 40)
+// 		document.body.style.background = "red";
+// });
+
+// addEventListener("keyup", function(event){
+// 	if (event.keyCode == 40)
+// 		document.body.style.background = "";
+// });
